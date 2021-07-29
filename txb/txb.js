0d17268b95f7a1d1c11684d9cfaeb74beae055a2
jimmyQxQ <85978813+jimmyQxQ@users.noreply.github.com>
2021-07-29 20:59:07 +0800

    Update txb.js

--- txb/txb.js
+++ txb/txb.js
@@ -1,13 +1,2 @@
-/*
-
-提供未测试qx规则
-hostname=*.tuxiaobei.com
-
-#兔小贝系列
-https://(.+)-api.tuxiaobei.com/v1/user/.+ * url script-response-body txb.js
-*/
-
-
-
 body = $response.body.replace(/vip":0/g, 'vip":0').replace(/vip_expire_at":null/g, 'vip_expire_at":"1800000000"').replace(/vip_type":""/g, 'vip_type":"2"');
 $done({body}); 
